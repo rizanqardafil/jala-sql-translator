fca6ba40a0deb49665f04bf186c8e7ef248cf180
Rizan Qardafil <rizanqardafil14@gmail.com>
2023-07-30 19:21:14 +0700

    fixing(frontend SQL Query)

---
+++ components/Header.tsx
@@ -0,0 +1,18 @@
+import Image from 'next/image'
+import PineconeLogo from '../public/Logo-Jala-Tech.svg'
+
+export default function Header({ className }: { className?: string }) {
+  return (
+    <header
+      className={`flex items-center justify-center text-gray-200 text-2xl ${className}`}
+    >
+      <Image
+        src={PineconeLogo}
+        alt='pinecone-logo'
+        width='230'
+        height='50'
+        className='ml-3'
+      />{' '}
+    </header>
+  )
+}
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,37 +1,13 @@
-import Document, { Head, Html, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document'
 
-class MyDocument extends Document {
-  render() {
-    return (
-      <Html lang='en'>
-        <Head>
-          <link rel='icon' href='/favicon.ico' />
-          <meta
-            name='description'
-            content='Translate human language to SQL queries in seconds.'
-          />
-          <meta property='og:site_name' content='SQL Translator' />
-          <meta
-            property='og:description'
-            content='Translate human language to SQL queries in seconds.'
-          />
-          <meta property='og:title' content='SQL Translator' />
-          <meta name='twitter:card' content='summary_large_image' />
-          <meta name='twitter:title' content='SQL Translator' />
-          <meta
-            name='twitter:description'
-            content='Translate human language to SQL queries in seconds.'
-          />
-        </Head>
-        <body className='dark:bg-black bg-gray-100 text-gray-800 dark:text-white font-sans transition-colors'>
-          <div className='max-w-4xl mx-auto px-4'>
-            <Main />
-          </div>
-          <NextScript />
-        </body>
-      </Html>
-    )
-  }
+export default function Document() {
+  return (
+    <Html lang='en' className='h-full'>
+      <Head />
+      <body className='bg-slate-800 h-full px-4'>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
 }
-
-export default MyDocument
